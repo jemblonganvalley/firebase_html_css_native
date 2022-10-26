2b3ce77e1a4e9a9b4063b3abbcc795ec773a9993
jemblonganvalley <56766364+jemblonganvalley@users.noreply.github.com>
2022-10-26 12:32:42 +0700

    remove unussual

--- main.js
+++ main.js
@@ -1,21 +1,5 @@
 import './style.css'
-import javascriptLogo from './javascript.svg'
-import { setupCounter } from './counter.js'
-import axios from 'axios'
 const page = window.location.pathname
-
-
-const getHtml = async()=>{
-  const result = await axios.get('https://vitejs-vite-3kktfw--5173.local.webcontainer.io/home.html', {
-    headers : {
-      "Content-Type" : "text/html"
-    }
-  })
-
-  return result.data
-  
-}
-
 
 switch(page){
   case "/":
